418ddd9c9c9692e71a621b0047b0d266029ad60d
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-23 15:43:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,22 +4,27 @@
   <link rel="stylesheet" type = "text/css" href = "css/style.css"/>
 </head>  
 <body>  
-    <h1>   Given a Positive Integer n, Find The Value of Factorial of a Number k </h1>
+    		<div class = "left">
 <form method="post">  
     Enter the Number:<br>  
     <input type="number" name="number" id="number">  
     <input type="submit" name="submit" value="Submit" />  
-</form>  
+</form> 
+  </div>
+   					<div class = "right">
 <?php   
     if($_POST){  
         $fact = 1;  
+        //getting value from input text box 'number'  
         $number = $_POST['number'];  
         echo "Factorial of $number:<br><br>";  
+        //start loop  
         for ($i = 1; $i <= $number; $i++){         
             $fact = $fact * $i;  
             }  
             echo $fact . "<br>";  
     }  
 ?>  
+  </div>
 </body>  
 </html>  
