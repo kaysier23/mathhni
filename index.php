a448a409f4732da846b670d7f206753f6636f87d
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-23 15:55:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
   <link rel="stylesheet" type = "text/css" href = "css/style.css"/>
 </head>  
 <body>  
-    		<div class = "left">
+  <h1>   Given a Positive Integer n, Find The Value of Factorial of a Number k </h1>
+ 					<div class = "left">
+            <h2> INPUT </h2>
 <form method="post">  
     Enter the Number:<br>  
     <input type="number" name="number" id="number">  
@@ -12,6 +14,7 @@
 </form> 
   </div>
    					<div class = "right">
+              <h3> OUTPUT </h3>
 <?php   
     if($_POST){  
         $fact = 1;  
